1b208451a7824a26e1549a107da5b7beb69381e1
IkeVoodoo <simone.coletti07@gmail.com>
2022-07-28 15:29:48 +0200

    Add default methods

--- src/main/java/dev/refinedtech/configlang/ConfigSection.java
+++ src/main/java/dev/refinedtech/configlang/ConfigSection.java
@@ -20,7 +20,11 @@ public abstract class ConfigSection {
 
     public abstract <T> Optional<T> get(String key);
 
+    public abstract <T> Optional<T> get(String key, T def);
+
     public abstract Object getObject(String key);
+
+    public abstract Object getObject(String key, Object def);
 
     public abstract void set(String key, Object value);
 
